ddb718abd693fe36e74daa54acedabb814540473
JMiguelDB <jose.miguel.db.17894@gmail.com>
2018-10-04 17:21:05 +0200

    Example project

---
+++ Project_cube-run/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeGameUI;
+
+    public void EndGame(){
+        if(!gameHasEnded){
+            Debug.Log("GAME OVER");
+            gameHasEnded = true;
+            //Invoca el metodo nombrado tras N segundos
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void CompleteGame(){
+        Debug.Log("Game completed");
+        completeGameUI.SetActive(true);
+    }
+
+    void Restart(){
+        //Carga la escena activa
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Project_cube-run/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+    // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if(Input.GetKey(KeyCode.D)){
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }else if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        //Comprobamos si se ha caido
+        if(rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+	}
+}
